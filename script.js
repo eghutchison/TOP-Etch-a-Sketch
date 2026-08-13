977c94150628599ceda6bdbdaee90e604dc9a4ff
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2026-08-13 16:04:25 -0700

    Etch-a-Sketch completed without Extra Credit components

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#reset");
+let side = 16;
 
-for (let i=0; i <256; i++) {
-    let square = document.createElement("div");
-    container.append(square);
+createGrid(side);
+
+//let mouseState = true;
+//document.addEventListener("mouseup", () => {mouseState = true});
+//document.addEventListener("mousedown", () => {mouseState = false});
+
+btn.addEventListener("click", () => {side = prompt("How many squares to a side? (Max. 100)"); createGrid(Math.min(100,side));})
+
+
+function createGrid (side) {
+    container.replaceChildren();
+    console.log(side);
+    for (let i=0; i < side ** 2; i++) {
+        let square = document.createElement("div");
+        square.style.width = (100/side)+"%"
+        square.style.height = (100/side)+"%"
+        container.append(square);
+        square.addEventListener("mouseover", () => /*{if (mouseState === true)*/ {square.style.backgroundColor = 'white'}/*}*/);
+}
 }
