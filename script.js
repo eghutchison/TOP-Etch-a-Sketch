e0d68058050bf90ae0a2c2c9a96f71a04940f903
Eric Hutchison <79342284+eghutchison@users.noreply.github.com>
2026-08-20 09:52:49 -0400

    Extra Credit 2 added for Opacity check

--- script.js
+++ script.js
@@ -13,14 +13,16 @@ btn.addEventListener("click", () => {side = prompt("How many squares to a side? 
 
 function createGrid (side) {
     container.replaceChildren();
-    console.log(side);
     for (let i=0; i < side ** 2; i++) {
         let square = document.createElement("div");
+        let r = Math.round(Math.random() * 255);
+        let g = Math.round(Math.random() * 255);
+        let b = Math.round(Math.random() * 255);
+        let x = 0;
         square.style.width = (100/side)+"%"
         square.style.height = (100/side)+"%"
         container.append(square);
-        square.addEventListener("mouseover", () => /*{if (mouseState === true)*/ {let r = Math.round(Math.random() * 255);
-        let g = Math.round(Math.random() * 255);
-        let b = Math.round(Math.random() * 255);square.style.backgroundColor = `rgb(${r},${g},${b})`;}/*}*/);
+        square.addEventListener("mouseover", () => /*{if (mouseState === true)*/ {
+        square.style.backgroundColor = `rgba(${r},${g},${b},${Math.min(x += 0.1, 1)})`;}/*}*/);
 }
 }
